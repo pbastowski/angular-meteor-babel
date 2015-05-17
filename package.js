a9a4b4ab1eee8ca95c0d590feff1dda1d52d5d2a
Paul Bastowski <Paul.Bastowski@klm.com>
2015-05-17 15:33:04 +0200

    updating to Babel v5.4.3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name:    'pbastowski:angular-babel',
     summary: 'Write javascript ES6 in your Angular-Meteor app',
-    version: '0.1.3',
+    version: '0.1.4',
     git:     'https://github.com/pbastowski/angular-meteor-babel.git'
 });
 
@@ -13,7 +13,7 @@ Package.registerBuildPlugin({
         'plugin/compile-6to5.js'
     ],
     npmDependencies: {
-        'babel-core':  '5.2.6',
+        'babel-core':  '5.4.3',
         'ng-annotate': '0.15.4'
     }
 });
