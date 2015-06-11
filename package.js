494d9097b5be7d54feb5cafbe37b9abaee6356db
Paul Bastowski <pbastowski@gmail.com>
2015-06-12 01:08:33 +0200

    Bumped version to 0.1.6

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name:    'pbastowski:angular-babel',
     summary: 'Write javascript ES6 in your Angular-Meteor app',
-    version: '0.1.5',
+    version: '0.1.6',
     git:     'https://github.com/pbastowski/angular-meteor-babel.git'
 });
 
