1d4b52d484d479800fb3b78c108eb76fdf9946f2
Paul Bastowski <pbastowski@gmail.com>
2015-12-31 01:20:52 +0100

    v1.0.8 Fix for missing browser-polyfill on server side.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name:    "pbastowski:angular-babel",
     summary: "Babel compiler and ng-annotate for Meteor 1.2",
-    version: "1.0.7",
+    version: "1.0.8",
     git:     "https://github.com/pbastowski/angular-meteor-babel.git"
 });
 
@@ -11,7 +11,7 @@ Package.registerBuildPlugin({
         'plugin/ng-script-compiler.js'
     ],
     npmDependencies: {
-        'babel-core':  '5.8.25',
+        'babel-core':  '5.8.33',
         'ng-annotate': '1.0.2'
     }
 });
@@ -32,12 +32,12 @@ Package.onUse(function (api) {
         // if it has already been run. I need this version to run and to
         // overwrite Meteor's bundled version, which is loaded in  memory.
         // Meteor's version does NOT include the async function helpers.
-        'lib/browser-polyfill.js',
+        //'lib/browser-polyfill.js',
 
         // Need this to support es7 async functions and generators
-        //'.npm/plugin/compileNGScript/node_modules/babel-core/browser-polyfill.js',
+        '.npm/plugin/compileNGScript/node_modules/babel-core/browser-polyfill.js',
 
-    ], 'client', { transpile: false });
+    ], ['client', 'server'], { transpile: false });
 });
 
 Package.onTest(function (api) {
