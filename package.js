b90449a80f53ca770e8af1c709c50f7bed97398e
Paul Bastowski <pbastowski@gmail.com>
2016-01-03 12:24:11 +0100

    Prevent browser-polyfill.js from polyfilling Number if it already exists. This is an attempt to fix the problem experienced by some users (angular-meteor issue #1015, angular-babel issue #12)

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name:    "pbastowski:angular-babel",
     summary: "Babel compiler and ng-annotate for Meteor 1.2",
-    version: "1.0.8",
+    version: "1.0.9",
     git:     "https://github.com/pbastowski/angular-meteor-babel.git"
 });
 
@@ -32,10 +32,10 @@ Package.onUse(function (api) {
         // if it has already been run. I need this version to run and to
         // overwrite Meteor's bundled version, which is loaded in  memory.
         // Meteor's version does NOT include the async function helpers.
-        //'lib/browser-polyfill.js',
+        'lib/browser-polyfill.js',
 
         // Need this to support es7 async functions and generators
-        '.npm/plugin/compileNGScript/node_modules/babel-core/browser-polyfill.js',
+        //'.npm/plugin/compileNGScript/node_modules/babel-core/browser-polyfill.js',
 
     ], ['client', 'server'], { transpile: false });
 });
